dcedc133e4575e97a2219fd6781c590d6f6a750b
App-Developer <rafiullahkhaleel@gmail.com>
2025-03-21 04:54:52 -0700

    commit

--- lib/screen/task_screen.dart
+++ lib/screen/task_screen.dart
@@ -11,7 +11,6 @@ class TaskScreen extends StatefulWidget {
 class _TaskScreenState extends State<TaskScreen> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
           title: Text(
@@ -42,21 +41,32 @@ class _TaskScreenState extends State<TaskScreen> {
           return Divider();
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context){
-                return CustomDialog();
-              });
-        },
-        shape: CircleBorder(),
-        backgroundColor: primary,
-        child: Icon(
-          Icons.add,
-          color: secondary,
-          size: 40,
-        ),
+      floatingActionButton: CustomFloatingButton(),
+    );
+  }
+}
+
+class CustomFloatingButton extends StatelessWidget {
+  const CustomFloatingButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return CustomDialog();
+            });
+      },
+      shape: CircleBorder(),
+      backgroundColor: primary,
+      child: Icon(
+        Icons.add,
+        color: secondary,
+        size: 40,
       ),
     );
   }
@@ -70,12 +80,46 @@ class CustomDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sh = MediaQuery.of(context).size.height;
-    final wh = MediaQuery.of(context).size.width;
+    final sw = MediaQuery.of(context).size.width;
     return Dialog(
       child: SizedBox(
-        height: sh*.6,
-        width: wh*.7,
-        child: Column(),
+        height: sh * .6,
+        width: sw * .7,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: sw * .05, vertical: sh * .02),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text('New Task',
+                    style: TextStyle(
+                        color: accent,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
+              ),
+              SizedBox(
+                height: sh * .02,
+              ),
+              Text(
+                'What has to be done',
+                style: TextStyle(color: accent, fontSize: 15),
+              ),
+              SizedBox(
+                height: sh * .02,
+              ),
+              SizedBox(
+                height: 25,
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Enter a Task',
+                      hintStyle: TextStyle(color: Colors.black45)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
